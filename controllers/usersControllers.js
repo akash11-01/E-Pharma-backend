c443d17354b4d348790379a5428b6af8e97c865b
Akash Jaiswar <akashjaiswar221@gmai.com>
2025-01-15 23:25:28 +0530

    remove email verification and password reset functionality

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -8,12 +8,6 @@ import {
   validateRefreshToken,
 } from "../services/tokenServices.js";
 import { User } from "../models/User.js";
-import {
-  sendPasswordResetEmail,
-  sendResetSuccessEmail,
-  sendVerificationEmail,
-  sendWelcomeEmail,
-} from "../mailtrap/emails.js";
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -24,58 +18,15 @@ export const registerUser = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const verificationToken = Math.floor(
-    100000 + Math.random() * 900000
-  ).toString();
 
   const newUser = await createUser({
     ...req.body,
     password: hashPassword,
-    verificationToken,
-    verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
   });
-
-  await sendVerificationEmail(newUser.email, verificationToken);
 
   res.status(201).json({
     user: { name: newUser.name, email: newUser.email },
   });
-};
-
-const verifyEmail = async (req, res) => {
-  const { code } = req.body;
-  try {
-    const user = await User.findOne({
-      verificationToken: code,
-      verificationTokenExpiresAt: { $gt: Date.now() },
-    });
-
-    if (!user) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid or expired verification code",
-      });
-    }
-
-    user.isVerified = true;
-    user.verificationToken = undefined;
-    user.verificationTokenExpiresAt = undefined;
-    await user.save();
-
-    await sendWelcomeEmail(user.email, user.name);
-
-    res.status(200).json({
-      success: true,
-      message: "Email verified successfully",
-      user: {
-        ...user._doc,
-        password: undefined,
-      },
-    });
-  } catch (error) {
-    // console.log("error in verifyEmail ", error);
-    res.status(500).json({ success: false, message: "Server error" });
-  }
 };
 
 const loginUser = async (req, res) => {
@@ -248,7 +199,4 @@ export default {
   refreshUser: ctrlWrapper(refreshUser),
   getCurrent: ctrlWrapper(getCurrent),
   logoutUser: ctrlWrapper(logoutUser),
-  resetPassword: ctrlWrapper(resetPassword),
-  forgotPassword: ctrlWrapper(forgotPassword),
-  verifyEmail: ctrlWrapper(verifyEmail),
 };
